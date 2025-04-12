052f0b2305eb78208e125f5414853140b2d5538c
yurii_fishbakh <bunbanto@gmail.com>
2025-04-12 14:29:11 +0200

    add foos prod-api

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,4 @@
 //Константи
 
 export const STORAGE_KEYS = {};
+export const BASE_URL = 'https://dummyjson.com';
--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,50 @@
 // Функції для роботи з бекендом
+
+import axios from 'axios';
+import { BASE_URL } from './constants';
+
+export async function getProductById(id) {
+  try {
+    const response = await axios.get(`${BASE_URL}/products/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function getProductByName(query) {
+  try {
+    const response = await axios.get(`${BASE_URL}/products/search?q=${query}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function getAllCategory() {
+  try {
+    const response = await axios.get(`${BASE_URL}/products/category-list`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function getProductsByCategory(category) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/products/category/${category}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getAllProducts(currentPage) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/products?limit=12&skip=${(currentPage - 1) * 12}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
